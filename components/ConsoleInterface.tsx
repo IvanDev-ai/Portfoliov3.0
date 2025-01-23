901a1328f8263214a02cafa791d29de10a4f6eb5
IvanDev <123570888+IvanDev-ai@users.noreply.github.com>
2025-01-23 21:40:51 +0100

    Update

--- components/ConsoleInterface.tsx
+++ components/ConsoleInterface.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState, useRef, useEffect } from 'react';
+import React, { useState} from 'react';
 import Slider from "react-slick";
 interface ConsoleOption {
   label: string;
